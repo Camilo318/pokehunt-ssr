7f8f7c82a14e32ef21d9e1c60c76305c4afc3107
Camilo318 <camiloa0318@gmail.com>
2024-01-28 23:24:12 -0500

    Add new graphql operation to get pokemon by id

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatId(id: string) {
+  if (typeof id !== 'string') {
+    throw new Error('id is not a string')
+  }
+  return id.padStart(3, '0')
+}
